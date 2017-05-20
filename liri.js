978f7cc8b95546340c32b5567e2eef8d7dfb3d47
ighobverhai aror <ighobverhai aror>
2017-05-20 13:03:02 -0400

    updated

--- liri.js
+++ liri.js
@@ -45,7 +45,7 @@ var getMeSpotify = function(songName) {
 var getTweets = function() {
   var client = new twitter(keys.twitterKeys);
 
-  var params = { screen_name: 'usmanjamil83', count: 10 };
+  var params = { screen_name: 'ighoa', count: 10 };
 
   client.get('statuses/user_timeline', params, function(error, tweets, response) {
 
